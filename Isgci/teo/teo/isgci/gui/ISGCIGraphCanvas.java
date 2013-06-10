a33453d64187060c90ab709f4037b2952f017245
Matthias Kraus <kraus.matze@googlemail.com>
2013-06-10 17:43:56 +0200

    Edges werden jeweils einzeln gezeichnet

--- Isgci/teo/teo/isgci/gui/ISGCIGraphCanvas.java
+++ Isgci/teo/teo/isgci/gui/ISGCIGraphCanvas.java
@@ -29,6 +29,8 @@ import org.jgrapht.graph.SimpleDirectedGraph;
 import org.jgrapht.graph.DefaultEdge;
 
 import com.mxgraph.layout.hierarchical.mxHierarchicalLayout;
+import com.mxgraph.model.mxCell;
+import com.mxgraph.model.mxICell;
 import com.mxgraph.swing.mxGraphComponent;
 import com.mxgraph.view.mxGraph;
 
@@ -45,272 +47,267 @@ import teo.isgci.grapht.Inclusion;
 /**
  * A canvas that can display an inclusion graph.
  */
-public class ISGCIGraphCanvas extends
-        GraphCanvas<Set<GraphClass>, DefaultEdge> {
-    protected NodePopup nodePopup;
-    protected EdgePopup edgePopup;
-    protected Problem problem;
-    protected Algo.NamePref namingPref;
-    ISGCIMainFrame parent;
-    mxGraph graph;
+public class ISGCIGraphCanvas extends GraphCanvas<Set<GraphClass>, DefaultEdge> {
+	protected NodePopup nodePopup;
+	protected EdgePopup edgePopup;
+	protected Problem problem;
+	protected Algo.NamePref namingPref;
+	ISGCIMainFrame parent;
+	mxGraph graph;
 
-    /** Colours for different complexities */
-    public static final Color COLOR_LIN = Color.green;
-    public static final Color COLOR_P = Color.green.darker();
-    public static final Color COLOR_NPC = Color.red;
-    public static final Color COLOR_INTERMEDIATE = SColor.brighter(Color.red);
-    public static final Color COLOR_UNKNOWN = Color.white;
+	/** Colours for different complexities */
+	public static final Color COLOR_LIN = Color.green;
+	public static final Color COLOR_P = Color.green.darker();
+	public static final Color COLOR_NPC = Color.red;
+	public static final Color COLOR_INTERMEDIATE = SColor.brighter(Color.red);
+	public static final Color COLOR_UNKNOWN = Color.white;
 
+	public ISGCIGraphCanvas(ISGCIMainFrame parent, mxGraph graph) {
+		super(parent, parent.latex, new ISGCIVertexFactory(), null);
+		this.parent = parent;
+		problem = null;
+		this.graph = graph;
+		namingPref = Algo.NamePref.BASIC;
+		setWidthFunc(new NodeWidthFunc());
+		nodePopup = new NodePopup(parent);
+		edgePopup = new EdgePopup(parent);
+		add(nodePopup);
+		add(edgePopup);
+	}
 
-    public ISGCIGraphCanvas(ISGCIMainFrame parent, mxGraph graph) {
-        super(parent, parent.latex, new ISGCIVertexFactory(), null);
-        this.parent = parent;
-        problem = null;
-        this.graph = graph;
-        namingPref = Algo.NamePref.BASIC;
-        setWidthFunc(new NodeWidthFunc());
-        nodePopup = new NodePopup(parent);
-        edgePopup = new EdgePopup(parent);
-        add(nodePopup);
-        add(edgePopup);
-    }
-
-
-    /**
-     * Add the given graph to this canvas.
-     */
-    protected GraphView<Set<GraphClass>,DefaultEdge> addGraph(
-            SimpleDirectedGraph<Set<GraphClass>,DefaultEdge> g) {
-        GraphView<Set<GraphClass>,DefaultEdge> gv = super.addGraph(g);
-        for (NodeView<Set<GraphClass>,DefaultEdge> nv : gv.getNodeViews()) {
-            nv.setColor(complexityColor(nv.getNode()));
-            nv.setNameAndLabel(Algo.getName(nv.getNode(), namingPref)); 
-        }
-        return gv;
-    }
-    
-
-    /**
-     * Create a hierarchy subgraph of the given classes and draw it.
-     */
-    public void drawHierarchy(Collection<GraphClass> nodes) {
-    	
-        SimpleDirectedGraph<Set<GraphClass>, DefaultEdge> edgegraph =
-            Algo.createHierarchySubgraph(nodes);
-//        //System.err.println(graph);
-//        List<SimpleDirectedGraph<Set<GraphClass>,DefaultEdge>> list =
-//                GAlg.split(graph, DefaultEdge.class);
-//        //System.err.println(list);
-//    	drawGraphs(list);
-    	
-		Object parent = graph.getDefaultParent();
-		
-		graph.getModel().beginUpdate();
-		try
-		{
-//			Object v1 = graph.insertVertex(parent, null, "Hello", 20, 20, 80,
-//					30);
-//			Object v2 = graph.insertVertex(parent, null, "World!", 240, 150,
-//					80, 30);
-//			graph.insertEdge(parent, null, "Edge", v1, v2);
-			
-			for(GraphClass g:nodes){
-				graph.insertVertex(parent, g.getID(), g.toString(), 20, 20, 80, 30);
-			}
-			for(DefaultEdge e:edgegraph.edgeSet()){
-				graph.insertEdge(parent, null, null, edgegraph.getEdgeSource(e).iterator().next(), edgegraph.getEdgeTarget(e).iterator().next());
-			}
-			
+	/**
+	 * Add the given graph to this canvas.
+	 */
+	protected GraphView<Set<GraphClass>, DefaultEdge> addGraph(
+			SimpleDirectedGraph<Set<GraphClass>, DefaultEdge> g) {
+		GraphView<Set<GraphClass>, DefaultEdge> gv = super.addGraph(g);
+		for (NodeView<Set<GraphClass>, DefaultEdge> nv : gv.getNodeViews()) {
+			nv.setColor(complexityColor(nv.getNode()));
+			nv.setNameAndLabel(Algo.getName(nv.getNode(), namingPref));
 		}
-		finally
-		{
+		return gv;
+	}
+
+	/**
+	 * Create a hierarchy subgraph of the given classes and draw it.
+	 */
+	public void drawHierarchy(Collection<GraphClass> nodes) {
+
+		SimpleDirectedGraph<Set<GraphClass>, DefaultEdge> edgegraph = Algo
+				.createHierarchySubgraph(nodes);
+		// //System.err.println(graph);
+		// List<SimpleDirectedGraph<Set<GraphClass>,DefaultEdge>> list =
+		// GAlg.split(graph, DefaultEdge.class);
+		// //System.err.println(list);
+		// drawGraphs(list);
+
+		Object parent = graph.getDefaultParent();
+
+		graph.getModel().beginUpdate();
+		try {
+			// Object v1 = graph.insertVertex(parent, null, "Hello", 20, 20, 80,
+			// 30);
+			// Object v2 = graph.insertVertex(parent, null, "World!", 240, 150,
+			// 80, 30);
+			// graph.insertEdge(parent, null, "Edge", v1, v2);
+
+			List<mxICell> list = new ArrayList<mxICell>();
+			for (DefaultEdge edge : edgegraph.edgeSet()) {
+				Set<GraphClass> source = edgegraph.getEdgeSource(edge);
+				System.out.println(source);
+				Set<GraphClass> target = edgegraph.getEdgeTarget(edge);
+				System.out.println(target);
+				//##########################
+				
+				
+				
+				graph.insertEdge(
+						parent,
+						edge.toString(),
+						null,
+						graph.insertVertex(parent, source.iterator().next()
+								.getID(), source.iterator().next(), 20, 20, 80, 30),
+						graph.insertVertex(parent, target.iterator().next()
+								.getID(), target.iterator().next(), 20, 20, 80, 30));
+
+				
+				//###########################
+				mxHierarchicalLayout layout = new mxHierarchicalLayout(graph);
+				layout.execute(parent);
+			}
+		} finally {
+
 			graph.getModel().endUpdate();
 		}
-		mxHierarchicalLayout layout = new mxHierarchicalLayout(graph);
+
+		mxGraphComponent graphComponent = new mxGraphComponent(graph);
+
+		this.parent.drawingPane = graphComponent;
+	}
+	
+	private void insertEdge(){
 		
-        mxGraphComponent graphComponent = new mxGraphComponent(graph);
-        layout.execute(graphComponent);      
-        this.parent.drawingPane = graphComponent;
-        
-       System.out.println(getRootPane());
-        
-		
-    }
-    
-    
-    /**
-     * Returns all classes on the canvas (unsorted).
-     */
-    public List<GraphClass> getClasses() {
-        List<GraphClass> result = new ArrayList<GraphClass>();
-        for (GraphView<Set<GraphClass>,DefaultEdge> gv : graphs) {
-            for (NodeView<Set<GraphClass>,DefaultEdge> nv : gv.getNodeViews())
-                for (GraphClass gc : nv.getNode())
-                    result.add(gc);
-        }
-        return result;
-    }
-    
-    
-    /**
-     * Returns all names of all nodes.
-     */
-    public List<String> getNames() {
-        List<String> result = new ArrayList<String>();
-        for (GraphView<Set<GraphClass>,DefaultEdge> gv : graphs) {
-            for (NodeView<Set<GraphClass>,DefaultEdge> nv : gv.getNodeViews())
-                for (GraphClass gc : nv.getNode())
-                    result.add(gc.toString());
-        }
-        return result;
-    }
+	}
 
+	/**
+	 * Returns all classes on the canvas (unsorted).
+	 */
+	public List<GraphClass> getClasses() {
+		List<GraphClass> result = new ArrayList<GraphClass>();
+		for (GraphView<Set<GraphClass>, DefaultEdge> gv : graphs) {
+			for (NodeView<Set<GraphClass>, DefaultEdge> nv : gv.getNodeViews())
+				for (GraphClass gc : nv.getNode())
+					result.add(gc);
+		}
+		return result;
+	}
 
-    /**
-     * Set all nodes to their prefered names.
-     */
-    public void setPreferedNames() {
-        for (GraphView<Set<GraphClass>, DefaultEdge> gv : graphs)
-            for (NodeView<Set<GraphClass>, DefaultEdge> v : gv.getNodeViews())
-               v.setNameAndLabel(Algo.getName(v.getNode(), namingPref)); 
-    }
+	/**
+	 * Returns all names of all nodes.
+	 */
+	public List<String> getNames() {
+		List<String> result = new ArrayList<String>();
+		for (GraphView<Set<GraphClass>, DefaultEdge> gv : graphs) {
+			for (NodeView<Set<GraphClass>, DefaultEdge> nv : gv.getNodeViews())
+				for (GraphClass gc : nv.getNode())
+					result.add(gc.toString());
+		}
+		return result;
+	}
 
+	/**
+	 * Set all nodes to their prefered names.
+	 */
+	public void setPreferedNames() {
+		for (GraphView<Set<GraphClass>, DefaultEdge> gv : graphs)
+			for (NodeView<Set<GraphClass>, DefaultEdge> v : gv.getNodeViews())
+				v.setNameAndLabel(Algo.getName(v.getNode(), namingPref));
+	}
 
-    /**
-     * Find the NodeView for the given graph class or null if not found
-     */
-    public NodeView<Set<GraphClass>, DefaultEdge> findNode(GraphClass gc) {
-        for (GraphView<Set<GraphClass>,DefaultEdge> gv : graphs) {
-            for (NodeView<Set<GraphClass>, DefaultEdge> v : gv.getNodeViews())
-                if (v.getNode().contains(gc))
-                    return v;
-        }
-        return null;
-    }
-     
+	/**
+	 * Find the NodeView for the given graph class or null if not found
+	 */
+	public NodeView<Set<GraphClass>, DefaultEdge> findNode(GraphClass gc) {
+		for (GraphView<Set<GraphClass>, DefaultEdge> gv : graphs) {
+			for (NodeView<Set<GraphClass>, DefaultEdge> v : gv.getNodeViews())
+				if (v.getNode().contains(gc))
+					return v;
+		}
+		return null;
+	}
 
-    /**
-     * Bit of a hack to get all ISGCI stuff in one place:
-     * Set the appropriate properness of the given edgeview.
-     */
-    protected void setProperness(EdgeView<Set<GraphClass>,DefaultEdge> view) {
-        List<Inclusion> path = GAlg.getPath(DataSet.inclGraph,
-                view.getFrom().iterator().next(),
-                view.getTo().iterator().next());
-        view.setProper(Algo.isPathProper(path)  ||
-                Algo.isPathProper(Algo.makePathProper(path)));
-    }
+	/**
+	 * Bit of a hack to get all ISGCI stuff in one place: Set the appropriate
+	 * properness of the given edgeview.
+	 */
+	protected void setProperness(EdgeView<Set<GraphClass>, DefaultEdge> view) {
+		List<Inclusion> path = GAlg.getPath(DataSet.inclGraph, view.getFrom()
+				.iterator().next(), view.getTo().iterator().next());
+		view.setProper(Algo.isPathProper(path)
+				|| Algo.isPathProper(Algo.makePathProper(path)));
+	}
 
+	/**
+	 * Set coloring for p and repaint.
+	 */
+	public void setProblem(Problem p) {
+		if (problem != p) {
+			problem = p;
+			setComplexityColors();
+			repaint();
+		}
+	}
 
-    /**
-     * Set coloring for p and repaint.
-     */
-    public void setProblem(Problem p) {
-        if (problem != p) {
-            problem = p;
-            setComplexityColors();
-            repaint();
-        }
-    }
+	public Problem getProblem() {
+		return problem;
+	}
 
+	/**
+	 * Return the color for node considering its complexity for the active
+	 * problem.
+	 */
+	protected Color complexityColor(Set<GraphClass> node) {
+		if (problem == null)
+			return COLOR_UNKNOWN;
+		Complexity complexity = problem.getComplexity(node.iterator().next());
+		if (/* complexity == null || */complexity.isUnknown())
+			return COLOR_UNKNOWN;
+		if (complexity.betterOrEqual(Complexity.LINEAR))
+			return COLOR_LIN;
+		if (complexity.betterOrEqual(Complexity.P))
+			return COLOR_P;
+		if (complexity.equals(Complexity.GIC))
+			return COLOR_INTERMEDIATE;
+		if (complexity.likelyNotP())
+			return COLOR_NPC;
+		return COLOR_UNKNOWN;
+	}
 
-    public Problem getProblem() {
-        return problem;
-    }
+	/**
+	 * Set all nodes to the proper complexity color.
+	 */
+	public void setComplexityColors() {
+		for (GraphView<Set<GraphClass>, DefaultEdge> gv : graphs)
+			for (NodeView<Set<GraphClass>, DefaultEdge> v : gv.getNodeViews())
+				v.setColor(complexityColor(v.getNode()));
+	}
 
+	public void setNamingPref(Algo.NamePref pref) {
+		namingPref = pref;
+		setPreferedNames();
+		updateBounds();
+		repaint();
+	}
 
-    /**
-     * Return the color for node considering its complexity for the active
-     * problem.
-     */
-    protected Color complexityColor(Set<GraphClass> node) {
-        if (problem == null)
-            return COLOR_UNKNOWN;
-        Complexity complexity= problem.getComplexity(node.iterator().next());
-        if (/*complexity == null  ||*/  complexity.isUnknown())
-            return COLOR_UNKNOWN;
-        if (complexity.betterOrEqual(Complexity.LINEAR))
-            return COLOR_LIN;
-        if (complexity.betterOrEqual(Complexity.P))
-            return COLOR_P;
-        if (complexity.equals(Complexity.GIC))
-            return COLOR_INTERMEDIATE;
-        if (complexity.likelyNotP())
-            return COLOR_NPC;
-        return COLOR_UNKNOWN;
-    }
+	public Algo.NamePref getNamingPref() {
+		return namingPref;
+	}
 
+	/**
+	 * Center the canvas on the given NodeView.
+	 */
+	public void centerNode(NodeView<Set<GraphClass>, DefaultEdge> v) {
+		Point p = null;
 
-    /**
-     * Set all nodes to the proper complexity color.
-     */
-    public void setComplexityColors() {
-        for (GraphView<Set<GraphClass>, DefaultEdge> gv : graphs)
-            for (NodeView<Set<GraphClass>, DefaultEdge> v : gv.getNodeViews())
-               v.setColor(complexityColor(v.getNode()));
-    }
+		for (GraphView<Set<GraphClass>, DefaultEdge> gv : graphs) {
+			if ((p = gv.getNodeCenter(v)) != null) {
+				((ISGCIMainFrame) parent).centerCanvas(p);
+				return;
+			}
+		}
+	}
 
+	// ----------------------- MouseListener stuff --------------------------
 
-    public void setNamingPref(Algo.NamePref pref) {
-        namingPref = pref;
-        setPreferedNames();
-        updateBounds();
-        repaint();
-    }
+	protected boolean mousePopup(MouseEvent event) {
+		if (!event.isPopupTrigger())
+			return false;
 
-    public Algo.NamePref getNamingPref() {
-        return namingPref;
-    }
+		event.consume();
 
+		View v = getViewAt(event.getPoint());
+		if (v == null)
+			return true;
+		if (v instanceof NodeView) {
+			nodePopup.setNode((NodeView) v);
+			nodePopup.show(this, event.getX(), event.getY());
+		}
+		if (v instanceof EdgeView) {
+			edgePopup.setEdge((EdgeView) v);
+			edgePopup.show(this, event.getX(), event.getY());
+		}
+		return true;
+	}
 
-
-    /**
-     * Center the canvas on the given NodeView.
-     */
-    public void centerNode(NodeView<Set<GraphClass>, DefaultEdge> v) {
-        Point p = null;
-
-        for (GraphView<Set<GraphClass>,DefaultEdge> gv : graphs) {
-            if ((p = gv.getNodeCenter(v)) != null) {
-                ((ISGCIMainFrame) parent).centerCanvas(p);
-                return;
-            }
-        }
-    }
-
-
-    //----------------------- MouseListener stuff --------------------------
-
-    protected boolean mousePopup(MouseEvent event) {
-        if (!event.isPopupTrigger())
-            return false;
-
-        event.consume();
-        
-        View v = getViewAt(event.getPoint());
-        if (v == null)
-            return true;
-        if (v instanceof NodeView) {
-            nodePopup.setNode((NodeView) v);
-            nodePopup.show(this, event.getX(), event.getY());
-        }
-        if (v instanceof EdgeView) {
-            edgePopup.setEdge((EdgeView) v);
-            edgePopup.show(this, event.getX(), event.getY());
-        }
-        return true;
-    }
-
-
-    //----------------------- Width calculator --------------------------
-    class NodeWidthFunc implements IntFunction<Set<GraphClass> > {
-        /** Return the width of node */
-        public int execute(Set<GraphClass> node) {
-            NodeView<Set<GraphClass>, DefaultEdge> view = getView(node);
-            view.updateSize();
-            return view.getSize().width;
-        }
-    }
+	// ----------------------- Width calculator --------------------------
+	class NodeWidthFunc implements IntFunction<Set<GraphClass>> {
+		/** Return the width of node */
+		public int execute(Set<GraphClass> node) {
+			NodeView<Set<GraphClass>, DefaultEdge> view = getView(node);
+			view.updateSize();
+			return view.getSize().width;
+		}
+	}
 }
 
 /* EOF */
