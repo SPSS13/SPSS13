0a577330902e12b3ba05664420725d5ab3f8607f
FabianVollmer <fabi.vollmer@gmail.com>
2013-07-04 07:49:13 +0200

    Disabling show/hide sup/sub in kontextmenu (not in menubar though)

--- Isgci/teo/teo/isgci/gui/NodePopup.java
+++ Isgci/teo/teo/isgci/gui/NodePopup.java
@@ -13,7 +13,9 @@ package teo.isgci.gui;
 import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import javax.swing.JDialog;
@@ -22,178 +24,270 @@ import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
 
 import teo.XsltUtil;
+import teo.isgci.db.DataSet;
 import teo.isgci.gc.GraphClass;
+import teo.isgci.grapht.Inclusion;
 import teo.isgci.util.Utility;
 
 import com.mxgraph.model.mxCell;
 import com.mxgraph.view.mxGraph;
 
+/**
+ * 
+ *
+ */
 public class NodePopup extends JPopupMenu implements ActionListener {
-    /**
+	private static final long serialVersionUID = 2260105093000618855L;
+	ISGCIMainFrame parent;
+	private JMenuItem deleteItem, infoItem;
+	private JMenuItem miShowSuperclasses;
+	private JMenuItem miHideSuperclasses;
+	private JMenuItem miShowSubclasses;
+	private JMenuItem miHideSubclasses;
+	private JMenuItem miShowDetails;
+	private JMenuItem miAddSuperclasses;
+	private JMenuItem miAddSubclasses;
+	private JMenuItem miShowNeighbours;
+
+	JMenu nameItem;
+	// rework
+	mxCell cell;
+	mxGraph graph;
+
+	private static String CHANGENAME = "Name: ";
+
+	public NodePopup(ISGCIMainFrame parent, mxGraph graph) {
+		super();
+		this.parent = parent;
+		this.graph = graph;
+		// deleteItem = new JMenuItem("Delete");
+		add(infoItem = new JMenuItem("Information"));
+		add(miShowDetails = new JMenuItem("Show sidebar"));
+		addSeparator();
+		add(nameItem = new JMenu("Change name"));
+		addSeparator();
+		add(miShowSuperclasses = new JMenuItem("Show superclasses"));
+		add(miHideSuperclasses = new JMenuItem("Hide superclasses"));
+		addSeparator();
+		add(miShowSubclasses = new JMenuItem("Show subclasses"));
+		add(miHideSubclasses = new JMenuItem("Hide subclasses"));
+		addSeparator();
+		add(miAddSuperclasses = new JMenuItem("Add superclasses"));
+		add(miAddSubclasses = new JMenuItem("Add subclasses"));
+		add(miShowNeighbours = new JMenuItem("Add neighbours"));
+		addSeparator();
+		miHideSubclasses.addActionListener(this);
+		miHideSuperclasses.addActionListener(this);
+		miShowSubclasses.addActionListener(this);
+		miShowSuperclasses.addActionListener(this);
+		miShowNeighbours.addActionListener(this);
+		miShowDetails.addActionListener(this);
+		miAddSubclasses.addActionListener(this);
+		miAddSuperclasses.addActionListener(this);
+		infoItem.addActionListener(this);
+	}
+
+	public void setNode(mxCell cell) {
+		this.cell = cell;
+	}
+
+	/**
 	 * 
 	 */
-    private static final long serialVersionUID = 2260105093000618855L;
-    ISGCIMainFrame parent;
-    private JMenuItem deleteItem, infoItem;
-    private JMenuItem miShowSuperclasses;
-    private JMenuItem miHideSuperclasses;
-    private JMenuItem miShowSubclasses;
-    private JMenuItem miHideSubclasses;
-    private JMenuItem miShowDetails;
-    private JMenuItem miAddSuperclasses;
-    private JMenuItem miAddSubclasses;
-    private JMenuItem miShowNeighbours;
+	public void actionPerformed(ActionEvent event) {
+		Object source = event.getSource();
+		if (source == infoItem) {
+			JDialog d = new GraphClassInformationDialog(parent,
+					searchName(cell));
+			d.setLocation(50, 50);
+			d.pack();
+			d.setSize(800, 600);
+			d.setVisible(true);
+		} else if (event.getActionCommand().startsWith(CHANGENAME)) {
+			String fullname = event.getActionCommand().substring(
+					CHANGENAME.length());
+			graph.getModel().beginUpdate();
+			try {
+				GraphClassSet graphClassSet = (GraphClassSet) cell.getValue();
+				// search the label matching the selection
+				GraphClass label = null;
+				for (GraphClass gc : getAllClasses(cell)) {
 
-    JMenu nameItem;
-    // rework
-    mxCell cell;
-    mxGraph graph;
+					if (gc.toString().equals(fullname)) {
+						label = gc;
+					}
+				}
+				// set the label
+				graphClassSet.setLabel(label);
+				graph.updateCellSize(cell);
+				parent.graphCanvas.animateGraph();
+			} finally {
+				graph.getModel().endUpdate();
+			}
+		} else if (source == miShowDetails) {
+			parent.visibleHaken.setState(true);
+			parent.getContentPane().add("West", parent.sidebar);
+			parent.button2.setVisible(false);
+			parent.sidebar.setVisible(true);
+			parent.graphCanvas.setSidebarConent();
+		} else if (source == miShowSuperclasses) {
+			parent.graphCanvas.drawSuperSub(parent.graphCanvas
+					.getSuperNodes(cell));
+		} else if (source == miHideSuperclasses) {
+			parent.graphCanvas.deleteSuperSub(parent.graphCanvas
+					.getSuperNodes(cell));
+		} else if (source == miShowSubclasses) {
+			parent.graphCanvas.drawSuperSub(parent.graphCanvas
+					.getSubNodes(cell));
+		} else if (source == miHideSubclasses) {
+			parent.graphCanvas.deleteSuperSub(parent.graphCanvas
+					.getSubNodes(cell));
+		} else if (source == miShowNeighbours) {
+			parent.graphCanvas.drawNeighbours(cell);
+		} else if (source == miAddSubclasses) {
+			parent.graphCanvas.addSubclasses(cell);
+		} else if (source == miAddSuperclasses) {
+			parent.graphCanvas.addSuperclasses(cell);
+		}
+	}
 
-    private static String CHANGENAME = "Name: ";
+	public void show(Component orig, int x, int y) {
+		// reworked
+		// LatexGraphics latex = ISGCIMainFrame.latex;
+		Set<GraphClass> gcs = getAllClasses(cell);
+		int i = 0;
 
-    public NodePopup(ISGCIMainFrame parent, mxGraph graph) {
-        super();
-        this.parent = parent;
-        this.graph = graph;
-        // deleteItem = new JMenuItem("Delete");
-        add(infoItem = new JMenuItem("Information"));
-        add(miShowDetails = new JMenuItem("Show sidebar"));
-        addSeparator();
-        add(nameItem = new JMenu("Change name"));
-        addSeparator();
-        add(miShowSuperclasses = new JMenuItem("Show superclasses"));
-        add(miHideSuperclasses = new JMenuItem("Hide superclasses"));
-        addSeparator();
-        add(miShowSubclasses = new JMenuItem("Show subclasses"));
-        add(miHideSubclasses = new JMenuItem("Hide subclasses"));
-        addSeparator();
-        add(miAddSuperclasses = new JMenuItem("Add superclasses"));
-        add(miAddSubclasses = new JMenuItem("Add subclasses"));
-        add(miShowNeighbours = new JMenuItem("Add neighbours"));
-        addSeparator();
-        miHideSubclasses.addActionListener(this);
-        miHideSuperclasses.addActionListener(this);
-        miShowSubclasses.addActionListener(this);
-        miShowSuperclasses.addActionListener(this);
-        miShowNeighbours.addActionListener(this);
-        miShowDetails.addActionListener(this);
-        miAddSubclasses.addActionListener(this);
-        miAddSuperclasses.addActionListener(this);
-        infoItem.addActionListener(this);
-    }
+		// TODO: Document
+		miShowSuperclasses.setEnabled(superClassesMissing());
+		miShowSubclasses.setEnabled(subClassesMissing());
+		miHideSubclasses.setEnabled(parent.graphCanvas.getSubNodes(cell).size()>0&&!subClassesMissing());
+		miHideSuperclasses.setEnabled(parent.graphCanvas.getSuperNodes(cell).size()>0&&!superClassesMissing());
+//		miAddSubclasses.setEnabled(subClassesAvailable());
+//		miAddSuperclasses.setEnabled(superClassesAvailable());
+		
+		nameItem.removeAll();
+		nameItem.setEnabled(gcs.size() != 1);
+		JMenuItem[] mItem = new JMenuItem[gcs.size()];
+		// FIXME sort and render latex properly
+		for (GraphClass gc : gcs) {
+			nameItem.add(mItem[i] = new JMenuItem((Utility.getShortName(gc
+					.toString()))));
+			mItem[i].setActionCommand(CHANGENAME + gc.toString());
+			mItem[i].addActionListener(this);
+			i++;
+		}
 
-    public void setNode(mxCell cell) {
-        this.cell = cell;
-    }
+		super.show(orig, x, y);
+	}
 
-    public void actionPerformed(ActionEvent event) {
-        Object source = event.getSource();
-        if (source == infoItem) {
-            JDialog d = new GraphClassInformationDialog(parent,
-                    searchName(cell));
-            d.setLocation(50, 50);
-            d.pack();
-            d.setSize(800, 600);
-            d.setVisible(true);
-        } else if (event.getActionCommand().startsWith(CHANGENAME)) {
-            String fullname = event.getActionCommand().substring(
-                    CHANGENAME.length());
-            graph.getModel().beginUpdate();
-            try {
-                GraphClassSet graphClassSet = (GraphClassSet)cell.getValue();
-                // search the label matching the selection
-                GraphClass label = null;
-                for (GraphClass gc : getAllClasses(cell)) {
+	/**
+	 * A method for getting all GraphClasses a mxCell contains of.
+	 * 
+	 * @param c
+	 *            the mxCell whose GraphClasses should be returned
+	 * @return A Set of all GraphClasses contained in a mxCell
+	 * @author Fabian Vollmer
+	 * @date 24.06.2013
+	 */
+	public Set<GraphClass> getAllClasses(mxCell c) {
+		Set<GraphClass> result = new HashSet<GraphClass>();
+		result = ((GraphClassSet) c.getValue()).getSet();
+		return result;
+	}
 
-                    if (gc.toString().equals(fullname)) {
-                        label = gc;
-                    }
-                }
-                // set the label
-                graphClassSet.setLabel(label);
-                graph.updateCellSize(cell);
-                parent.graphCanvas.animateGraph();
-            } finally {
-                graph.getModel().endUpdate();
+	/**
+	 * Searches for the GraphClass represented by the name of a mxCell which is
+	 * a Latex String
+	 * 
+	 * @param c
+	 *            the mxCell that includes the name of the GraphClass that
+	 *            should be searched
+	 * @return the GraphClass represented by the Name
+	 * 
+	 * @author Fabian Vollmer
+	 * @date 24.06.2013
+	 */
+	public static GraphClass searchName(mxCell c) {
+		return ((GraphClassSet) c.getValue()).getLabel();
+	}
+
+	/**
+	 * Method for calculating if there are more superclasses for the selected
+	 * node in the drawn universe to show
+	 * 
+	 * @return true if there are more classes
+	 * @author Fabian Vollmer
+	 * @date 03.07.2013
+	 */
+	private boolean superClassesMissing() {
+		List<Object> l = new ArrayList<Object>();
+		if (parent.graphCanvas.getSuperNodes(cell).size() > 0) {
+			for (GraphClass gc : parent.graphCanvas.getSuperNodes(cell)) {
+				if(!((mxCell)parent.graphCanvas.findNode(gc)).isVisible()){
+					l.add(parent.graphCanvas.findNode(gc));
+				}
+			}
+		}
+		System.out.println();
+		return l.size() > 0 ? true : false;
+	}
+
+	/**
+	 * Method for calculating if there are more subclasses for the selected node
+	 * in the drawn universe to show
+	 * 
+	 * @return true if there are more classes
+	 * @author Fabian Vollmer
+	 * @date 03.07.2013
+	 */
+	private boolean subClassesMissing() {
+		List<Object> l = new ArrayList<Object>();
+		if (parent.graphCanvas.getSubNodes(cell).size() > 0) {
+			for (GraphClass gc : parent.graphCanvas.getSubNodes(cell)) {
+				if(!((mxCell)parent.graphCanvas.findNode(gc)).isVisible()){
+					l.add(parent.graphCanvas.findNode(gc));
+				}
+			}
+		}
+		System.out.println();
+		return l.size() > 0 ? true : false;
+	}
+
+	/**
+	 * Method for calculating if there are more superclasses for the selected
+	 * node to be drawn
+	 * 
+	 * @return true if there are more classes
+	 * @author Fabian Vollmer
+	 * @date 03.07.2013
+	 */
+	private boolean superClassesAvailable() {
+		// TODO:
+		return parent.graphCanvas.getSuperNodes(cell).size() > 0;
+	}
+
+	/**
+	 * Method for calculating if there are more subclasses for the selected node
+	 * to be drawn
+	 * 
+	 * @return true if there are more classes
+	 * @author Fabian Vollmer
+	 * @date 03.07.2013
+	 */
+	private boolean subClassesAvailable() {
+		//TODO:
+		List<GraphClass> l = new ArrayList<GraphClass>();
+		GraphClass gc = ((GraphClassSet)cell.getValue()).getLabel();
+		if (!DataSet.inclGraph.edgesOf(gc).isEmpty()) {
+            for (Inclusion inc : DataSet.inclGraph.edgesOf(gc)) {
+                l.add(inc.getSub());
+                System.out.println(inc.getSub());
             }
-        } else if (source == miShowDetails) {
-            parent.visibleHaken.setState(true);
-            parent.getContentPane().add("West", parent.sidebar);
-            parent.button2.setVisible(false);
-            parent.sidebar.setVisible(true);
-            parent.graphCanvas.setSidebarConent();
-        } else if (source == miShowSuperclasses) {
-            parent.graphCanvas.drawSuperSub(parent.graphCanvas
-                    .getSuperNodes(cell));
-        } else if (source == miHideSuperclasses) {
-            parent.graphCanvas.deleteSuperSub(parent.graphCanvas
-                    .getSuperNodes(cell));
-        } else if (source == miShowSubclasses) {
-            parent.graphCanvas.drawSuperSub(parent.graphCanvas
-                    .getSubNodes(cell));
-        } else if (source == miHideSubclasses) {
-            parent.graphCanvas.deleteSuperSub(parent.graphCanvas
-                    .getSubNodes(cell));
-        } else if (source == miShowNeighbours) {
-            parent.graphCanvas.drawNeighbours(cell);
-        } else if (source == miAddSubclasses) {
-            parent.graphCanvas.addSubclasses(cell);
-        } else if (source == miAddSuperclasses) {
-            parent.graphCanvas.addSuperclasses(cell);
-        }
-    }
-
-    public void show(Component orig, int x, int y) {
-        // reworked
-        // LatexGraphics latex = ISGCIMainFrame.latex;
-        Set<GraphClass> gcs = getAllClasses(cell);
-        int i = 0;
-
-        nameItem.removeAll();
-        nameItem.setEnabled(gcs.size() != 1);
-        JMenuItem[] mItem = new JMenuItem[gcs.size()];
-        // FIXME sort and render latex properly
-        for (GraphClass gc : gcs) {
-            nameItem.add(mItem[i] = new JMenuItem((Utility.getShortName(gc
-                    .toString()))));
-            mItem[i].setActionCommand(CHANGENAME + gc.toString());
-            mItem[i].addActionListener(this);
-            i++;
-        }
-
-        super.show(orig, x, y);
-    }
-
-    /**
-     * A method for getting all GraphClasses a mxCell contains of.
-     * 
-     * @param c
-     *            the mxCell whose GraphClasses should be returned
-     * @return A Set of all GraphClasses contained in a mxCell
-     * @author Fabian Vollmer
-     * @date 24.06.2013
-     */
-    public Set<GraphClass> getAllClasses(mxCell c) {
-        Set<GraphClass> result = new HashSet<GraphClass>();
-        result = ((GraphClassSet)c.getValue()).getSet();
-        return result;
-    }
-
-    /**
-     * Searches for the GraphClass represented by the name of a mxCell which is
-     * a Latex String
-     * 
-     * @param c
-     *            the mxCell that includes the name of the GraphClass that
-     *            should be searched
-     * @return the GraphClass represented by the Name
-     * 
-     * @author Fabian Vollmer
-     * @date 24.06.2013
-     */
-    public static GraphClass searchName(mxCell c) {
-        return ((GraphClassSet)c.getValue()).getLabel();
-    }
+        }		
+		System.out.println(l.size());
+		l.removeAll(((GraphClassSet)cell.getValue()).getSet());
+		return l.size() > 0;
+	}
 }
 
 /* EOF */
